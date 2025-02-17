1a3a4e34cdb182b7959cb6b51cee84d326ab34e4
yoshimoj <157053604+yoshimoj@users.noreply.github.com>
2025-02-16 19:27:56 -0800

    test4
    implemented lowercase check

--- check_pwd.py
+++ check_pwd.py
@@ -6,6 +6,12 @@ def check_pwd(password: str) -> bool:
     if not (8 <= len(password) <=20):
         return False
 
+    """"
+    Checks that there's at least one lowercase letter
+    """
+    if not any(c.islower() for c in password):
+        return False
+
     """
     If all tests are passed, return True
     """
