e5645ea8894e44d47cbb1e6ff43ec524ab79a7f3
yoshimoj <157053604+yoshimoj@users.noreply.github.com>
2025-02-16 19:09:02 -0800

    test2
    Testing for pwd that's too short, length must be 8 characters or more.

--- tests.py
+++ tests.py
@@ -2,8 +2,18 @@ import unittest
 from check_pwd import check_pwd
 
 class TestCheckPwd(unittest.TestCase):
-    def test_empty_string(self):
+    """
+    Testing for empty pwd
+    """
+    def test1(self):
         self.assertFalse(check_pwd(""))
+
+    """
+    Testing for pwd that's too short,
+    length must be 8 characters or more.
+    """
+    def test2(self):
+        self.assertFalse(check_pwd("A1!bc"))
 
 if __name__ == "__main__":
     unittest.main()
