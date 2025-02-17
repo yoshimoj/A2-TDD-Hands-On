bbba5fcd7b461f3890c8fc5477b82e8b0cfa8f6a
yoshimoj <157053604+yoshimoj@users.noreply.github.com>
2025-02-16 19:14:30 -0800

    test2
    checks that the length of password is 8-20 characters

--- check_pwd.py
+++ check_pwd.py
@@ -1,2 +1,12 @@
 def check_pwd(password: str) -> bool:
-    return len(password) > 0
+    """
+    Checks for correct password length
+    (8-20 characters)
+    """
+    if not (8 <= len(password) <=20):
+        return False
+
+    """
+    If all tests are passed, return True
+    """
+    return True
